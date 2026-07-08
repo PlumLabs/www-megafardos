599bebd44939e58267cabbbb00c8033c4d031114
saulcoria <coriasaul@gmail.com>
2026-07-08 18:23:40 -0300

    Add landingA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/landing-1',
+        destination: '/landing-1.html',
+      },
+    ]
+  },
+}
 module.exports = nextConfig
